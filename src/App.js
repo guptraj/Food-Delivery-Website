4027ad98f5892b2175bbeeae6fd76fd4141164fc
Rajat <guptraj15@gmail.com>
2022-10-11 17:20:22 +0530

    other components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { CreateContainer, Header, MainContainer } from "./components";
- import { AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
+import { useStateValue } from "./context/StateProvider";
+import { getAllFoodItems } from "./utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
 
 const App = () => {
+  const [{ foodItems }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllFoodItems().then((data) => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <div class="w-screen h-auto flex flex-col bg-primary">
         <Header />
 
